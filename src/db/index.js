d904ef41a933f39c80823106a052e3a4a6cf6586
itzrahul02 <rahulbikker@gmail.com>
2025-08-25 02:44:13 +0530

    Initial commit

--- src/db/index.js
+++ src/db/index.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js"
 const connectDB = async()=>{
     try{
-        await mongoose.connect(`mongodb+srv://rahulbikker:V6Cm9DMkNeUD81AX@backend.bwbiiit.mongodb.net/`)
+        await mongoose.connect(process.env.MONGO_URL)
         console.log("Connected to MongoDB");
     }
     catch(err){
