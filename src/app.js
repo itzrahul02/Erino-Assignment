e8f489f9f95dbaff34e6a13db38a42fd34cdfe9a
itzrahul02 <rahulbikker@gmail.com>
2025-08-25 17:04:31 +0530

    try 6

--- src/app.js
+++ src/app.js
@@ -10,11 +10,10 @@ dotenv.config()
 const app = express()
 
 app.use(cors({
-    origin: [            
-        process.env.CORS_ORIGIN  
-        // 'http://localhost:5173'               
-    ],
-    credentials: true
+  origin: process.env.CORS_ORIGIN,
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization", "x-refresh-token"]
 }));
 app.use(express.json({
     limit:"16kb"
