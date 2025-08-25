53702b09b2c2fcf02bd5bab7472657b40ab80571
itzrahul02 <rahulbikker@gmail.com>
2025-08-25 17:01:02 +0530

    try 5

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const app = express()
 
 app.use(cors({
     origin: [            
-        'https://erino-assignment-gilt.vercel.app/'  
+        process.env.CORS_ORIGIN  
         // 'http://localhost:5173'               
     ],
     credentials: true
